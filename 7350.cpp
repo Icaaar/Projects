20bfff0a8487f865c349f354825ec891b8822751
Icaaar <35259554+Icaaar@users.noreply.github.com>
2018-04-12 19:46:16 +1000

    Такая же задача, как и 7318, только вместо 9 ставим 1

---
+++ 7350.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A,b,c;
+    cin >> A;
+    c = 0;
+    b = 0;
+    while (A>0)
+    {
+    b = A%10;
+    if (b != 9)
+    {
+    c = c*10;
+    c = c+b;
+    }
+    A = A/10;
+    }
+    cout << c;
+
+}
